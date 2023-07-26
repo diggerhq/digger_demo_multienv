fec3d412a27a2cef21114892465d6eb9fae86a06
Igor Zalutski <izalutski@gmail.com>
2023-07-26 14:23:16 +0100

    Update main.tf

--- prod/main.tf
+++ prod/main.tf
@@ -2,6 +2,10 @@ resource "null_resource" "test_prod" {
 
 }
 
-resource "null_resource" "test_prod222" {
+resource "null_resource" "test_prod2" {
+
+}
+
+resource "null_resource" "test_prod3" {
 
 }
