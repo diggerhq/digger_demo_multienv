4d6f3c50a2d60f319847b30ad51bf98e4d04543a
Vitor Pellegrino <127766727+vitor-duolingo@users.noreply.github.com>
2023-09-25 12:47:26 +0200

    testing digger

--- dev/main.tf
+++ dev/main.tf
@@ -1,11 +1,10 @@
-resource "null_resource" "test_dev" {
+resource "null_resource" "dev" {
+}
+
+resource "null_resource" "testing_digger" {
 
 }
 
-resource "null_resource" "test_de222222262" {
-
-}
-
-resource "null_resource" "test_de333" {
+resource "null_resource" "test" {
 
 }
