508e8614f970c8959e85e1682f1c5ce1c6a833ec
AppliNH <applinh@protonmail.com>
2023-10-11 16:36:14 +0800

    chore(dev): add s3 backend

--- dev/backend.tf
+++ dev/backend.tf
@@ -1,1 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 
+  backend "s3" {
+    bucket = "terraform-state-applinh"
+    key    = "dev/terraform.tfstate"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
